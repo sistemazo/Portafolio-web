c27c8c38bb45b34473f70fe16a931081815861ca
sistemazo <sistemazo>
2022-11-01 11:49:56 -0400

    experience view

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -4,7 +4,23 @@ import './experience.css'
 
 const Experience = () => {
     return (
-      <section id="experience">Experience</section>
+      <section id="experience">
+        <h5>What Skills I have</h5>
+        <h2>my experience</h2>
+
+        <div className='container experience__container'>
+          <div className="experience__frontend">
+            <h3>Frontend developement</h3>
+            <div className='experience__content'>
+
+            </div>
+          </div>
+
+        <div className='experience__backend'>
+
+        </div>
+        </div>
+      </section>
     )
   }
   
