4d61388b058464b2ed93921516d341d440c2ba06
sistemazo <sistemazo>
2022-11-09 11:11:31 -0400

    new modify

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import {BsLinkedin} from 'react-icons/bs'
-import {BsGithub} from 'react-icons/bs' 
-import {RiInstagramFill} from 'react-icons/ri' 
-
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
+import { RiInstagramFill } from "react-icons/ri";
 
 const HeaderSocials = () => {
-    return (
-      <div className='header__sociales'>
-        <a href="https://linkedin.com" target="_blark"><BsLinkedin/></a>
-        <a href="https://github.com" target="_blark"><BsGithub/></a>
-        <a href="" target="_blark"><RiInstagramFill/></a>
-      </div>
-    )
-  }
-  
-  export default HeaderSocials
+  return (
+    <div className="header__sociales">
+      <a href="https://linkedin.com" target="_blark">
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com" target="_blark">
+        <BsGithub />
+      </a>
+      <a href="" target="_blark">
+        <RiInstagramFill />
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+import "./index.css";
+import App from "./App";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-import './index.css';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const element = <App/>;
-
-root.render(element)
-
-
-
-
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
