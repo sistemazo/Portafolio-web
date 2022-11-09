4727eb7f55ffc841ce83e637c8f4cae0d8174001
sistemazo <sistemazo>
2022-11-09 11:11:09 -0400

    useState

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import './header.css'
-import CTA from './CTA'
-import ME from '../../assets/me.png'
-import HeaderSocials from './HeaderSocials'
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/me.png";
+import HeaderSocials from "./HeaderSocials";
 const Header = () => {
-    return (
-      <header>
-        <div className='container header__container'>
-           <h5>Hola soy</h5>
-           <h1>Kervin Hernandez</h1>
-           <h5 className="text-light">Fullstack Developer</h5>
-           <CTA/>
-           <HeaderSocials/>
-           <div className="me">
-            <img src={ME} alt="me" />
-           </div>
-           <a href="#contact" className='scroll__down'>scroll down</a>
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hola soy</h5>
+        <h1>Kervin Hernandez</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={ME} alt="me" />
         </div>
-      </header>
-    )
-  }
-  
-  export default Header
+        <a href="#contact" className="scroll__down">
+          scroll down
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,26 +1,54 @@
-import React from 'react'
-import './nav.css'
-import {AiFillHome} from 'react-icons/ai'
-import {FaUserCircle} from 'react-icons/fa'
-import {BiBook} from 'react-icons/bi'
-import {FcServices} from 'react-icons/fc'
-import {BiMessage} from 'react-icons/bi'
-import { useState } from 'react'
-
+import React from "react";
+import "./nav.css";
+import { AiFillHome } from "react-icons/ai";
+import { FaUserCircle } from "react-icons/fa";
+import { BiBook } from "react-icons/bi";
+import { FcServices } from "react-icons/fc";
+import { BiMessage } from "react-icons/bi";
+import { useState } from "react";
 
 const Nav = () => {
-    const [activeNav,SetactiveNav] = useState('#')
-    
-    return (
-      <nav>
-        <a href="#" onClick={() =>SetactiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiFillHome/></a>
-        <a href="#about" onClick={() => SetactiveNav('#about')} className={activeNav === '#about' ? 'active' : '' }><FaUserCircle/></a>
-        <a href="#experience" onClick={() => SetactiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
-        <a href="#services" onClick={() => SetactiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><FcServices/></a>
-        <a href="#contact" onClick={() => SetactiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessage/></a>
-      </nav> 
-    
-    )
-  }
-  
-  export default Nav
+  const [activeNav, SetactiveNav] = useState("#");
+
+  return (
+    <nav>
+      <a
+        href="#"
+        onClick={() => SetactiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <AiFillHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => SetactiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <FaUserCircle />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => SetactiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <BiBook />
+      </a>
+      <a
+        href="#services"
+        onClick={() => SetactiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
+        <FcServices />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => SetactiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <BiMessage />
+      </a>
+    </nav>
+  );
+};
+
+export default Nav;
